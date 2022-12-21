a5c70033e7512d27a406e54ac6f0ee3732dbfd8f
Marwa238 <121182346+Marwa238@users.noreply.github.com>
2022-12-21 19:16:19 +0200

    Add files via upload

---
+++ perfect number/perfect number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace perfect_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            for(int i=Math.Min(num1 ,num2);i<=Math.Max(num1,num2);i++ )
+            {
+                int sum = 0;
+                for(int j=1;j<i;j++)
+                {
+                    if (i % j == 0)
+                        sum += j;
+                }
+                if (sum == i)
+                    Console.WriteLine(i + "is a perfect number");
+            }
+               
+        }
+    }
+}
---
+++ prime number/prime number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            for (int i = Math.Min(num1, num2); i <= Math.Max(num1, num2); i++)
+            {
+                bool prime = true;
+                for(int j=2;j<i;j++)
+                {
+                    if (i % j == 0)
+                        prime = false;
+                }
+
+                if (prime == true && i > 1)
+                    Console.WriteLine(i + "is prime");
+
+            }
+
+        }
+    }
+}
